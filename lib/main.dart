bedd9f5355bc91533998370bfe56a34e47d60184
ahmedraza55592 <ahmedraza55592@gmail.com>
2021-03-23 02:41:42 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "This is Todo App"
+        ),
+      ),
+      
+    );
+  }
+}
